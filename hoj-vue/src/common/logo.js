650802d7eb960d0a39459ccfe466df149d9c2ce4
NewOnlineJudge <132434997+NewOnlineJudge@users.noreply.github.com>
2023-05-03 15:23:05 +0800

    更新 logo

--- hoj-vue/src/common/logo.js
+++ hoj-vue/src/common/logo.js
@@ -1,16 +1,17 @@
 export const LOGO = 
-"          _    _  _____ ____  _____  ______    \n"+
-"         | |  | |/ ____/ __ \|  __ \|  ____|   \n"+
-"         | |__| | |   | |  | | |  | | |__      \n"+
-"         |  __  | |   | |  | | |  | |  __|     \n"+
-"         | |  | | |___| |__| | |__| | |____    \n"+
-"         |_|  |_|\_____\____/|_____/|______|   \n"+
-"              Hcode Online Judge(HOJ)"+"\n"+
-"                 @Author Himit_ZH"+"\n"+
-"                 @Last Update 20230501"+"\n"+
+"               _   _   ____        _ \n"+
+"              | \\ | | / __ \\      | |\n"+
+"              |  \\| || |  | |     | |\n"+
+"              | . ` || |  | | _   | |\n"+
+"              | |\\  || |__| || |__| |\n"+
+"              |_| \\_| \\____/  \\____/ \n"+
+"                  Powered by HOJ\n"+
+"              New Online Judge(NOJ)"+"\n"+
+"                 @Author Black.X"+"\n"+
+"                 @Last Update 20230503"+"\n"+
 "                QQ Group: 598587305"+"\n"+
-"    ->Github<- https://www.github.com/HimitZH/HOJ"+"\n"+
-"    ->Gitee<-  https://gitee.com/himitzh0730/hoj";
+"    ->Old NOJ<- Multiple bugs, disorderly order, lack of maintenance"+"\n"+
+"    ->New Online Judge<-  Security, Stability, Development";
 
 export const MOTTO = 
-"         Welcome to Use HOJ, Dream in Code!";
+"         Welcome to Use NOJ, Dream in Code!";
